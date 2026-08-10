93879da4de45256e1fd57cb382be05e61d89deb5
Pavel Vasilchenko <id.56pvp@icloud.com>
2026-08-10 10:31:09 +0300

    :wq

--- fit_life.py
+++ fit_life.py
@@ -1,26 +1,95 @@
 # Проект FitLife - MVP версия 1.0
 
 
-# 1. Знакомство
-# TODO: Спроси у пользователя имя и сохрани в переменную user_name
-# TODO: Спроси возраст и сохрани в переменную user_age (не забудь преобразовать в число)
+def greeting():
+    """Программа вежливо здоровается спрашивает имя и возраст"""
+    name = get_name("Здравствуйте, как Вас зовут?\n")
+    age = get_age("Сколько Вам лет?\n")
+    return name, age
 
 
-# 2. Сбор данных
-# TODO: Запроси вес (в кг) и сохрани в user_weight (тип float)
-# TODO: Запроси рост (в метрах, например 1.75) и сохрани в user_height (тип float)
+def get_name(prompt):
+    while True:
+        name = input(prompt).strip()
+        if not name:
+            print("Имя не может быть пустым. Пожалуйста, введите корректное имя.")
+            continue
+        return name
 
 
-# 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
-# Формула ИМТ: вес разделить на (рост в квадрате)
-# TODO: Рассчитай bmi (Индекс массы тела)
+def get_age(prompt):
+    while True:
+        try:
+            age = int(input(prompt))
+            if age <= 0:
+                print(
+                    "Возраст должен быть положительным числом. Пожалуйста, попробуйте снова."
+                )
+                continue
+            return age
+        except ValueError:
+            print("Пожалуйста, введите корректный возраст (целое число).")
 
 
-# Подсчет воды: вес * 30 мл
-# TODO: Рассчитай water_needed
+def parameters():
+    """Программа спрашивает вес и рост"""
+    weight = get_weight("Напишите Ваш вес в кг (например, 72)\n")
+    height = get_height("Напишите Ваш рост в метрах (например, 1.75)\n")
+    return weight, height
 
 
-# 4. Вывод красивого результата
-# TODO: Используй f-строку, чтобы вывести приветствие, например: "Привет, Иван!"
-# TODO: Выведи возраст, ИМТ (округленный до 1 знака) и норму воды.
-print("Расчет окончен. Будьте здоровы!")
+def get_weight(prompt):
+    while True:
+        try:
+            weight = int(input(prompt))
+            if weight <= 0:
+                print(
+                    "Вес должен быть положительным числом. Пожалуйста, попробуйте снова."
+                )
+                continue
+            return weight
+        except ValueError:
+            print("Пожалуйста, введите корректный вес (целое число).")
+
+
+def get_height(prompt):
+    while True:
+        try:
+            height = float(input(prompt))
+            if height <= 0:
+                print(
+                    "Рост должен быть положительным числом. Пожалуйста, попробуйте снова."
+                )
+                continue
+            return height
+        except ValueError:
+            print("Пожалуйста, введите корректный рост (например, 1.75).")
+
+
+def calculate_bmi(user_weight, user_height):
+    """Программа вычисляет Индекс массы тела (ИМТ)"""
+    # Индекс массы тела, Округлить
+    bmi = round(user_weight / (user_height**2), 1)
+    return bmi
+
+
+def calculate_water_needed(user_weight):
+    """Прорамма вычисляет норму воды в литрах"""
+    # Рассчитать норму воды в миллилитрах:
+    ML_OF_WATER_PER_KG = 30
+    water_ml = user_weight * ML_OF_WATER_PER_KG
+    # Перевести в литры (делим на 1000мл), округлить:
+    water_l = round(water_ml / 1000, 1)
+    return water_l
+
+
+def FitLife_MVP():
+    name, age = greeting()
+    weight, height = parameters()
+    print(f"\n\n\nОтчет для пользователя: {name} ({age} г.)")
+    print(f"Твой Индекс Массы Тела: {calculate_bmi(weight, height)}")
+    print(f"Рекомендуемая норма воды: {calculate_water_needed(weight)} л. в день")
+    print("\nРасчет окончен. Будьте здоровы!\n")
+
+
+FitLife_MVP()
